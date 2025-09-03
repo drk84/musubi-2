f2ab689bd15205b650acdc49df5551d639f9afb5
Pietro <pietro.piscioneri@gmail.com>
2025-09-03 14:45:44 +0200

    done with footer

--- js/musubi-2.js
+++ js/musubi-2.js
@@ -1,1 +1,20 @@
-  
+const options = {
+    root:null,
+    rootMargin:'0px 0px 0px 100%',
+    scrollMargin:'0px',
+    threshold: 0.25,
+};
+
+const callback = ( entries, observer ) => {
+ entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+         console.log(entries);
+         console.log('Element is in view:', entry.target);
+         entry.target.classList.add('trasla');
+   }
+ });
+};
+
+const observer = new IntersectionObserver(callback, options);
+const targets = document.querySelectorAll('main section');
+targets.forEach(target => observer.observe(target));
